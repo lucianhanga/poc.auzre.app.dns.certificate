0e2ce55898e0df7ed2d2c71cb47a8d5e78f81a8c
lucian h. <lucianhanga@gmx.net>
2024-10-06 15:25:49 +0000

    Refactor webapp.tf to disable always-on and update Python version to 3.11

--- terraform/webapp.tf
+++ terraform/webapp.tf
@@ -15,6 +15,7 @@ resource "azurerm_linux_web_app" "webapp" {
   resource_group_name = var.resource_group_name
   service_plan_id = azurerm_service_plan.webapp_service_plan.id
   site_config {
+    always_on = false
     application_stack {
         python_version = "3.11"
     }
